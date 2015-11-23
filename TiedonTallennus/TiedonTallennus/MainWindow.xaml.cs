a4b5ee2ebbb90260ef35fb1b8ff6f0dcd558dbfc
Johanna Merikanto <johanna.merikanto@stud.pointcollege.fi>
2015-11-23 15:03:23 +0200

    Ensimmäinen versio.

---
+++ TiedonTallennus/TiedonTallennus/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TiedonTallennus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static void Main(string[] args)
+        {
+            int lämpötila = 0;
+            //Console.Write("Anna uusi lämpötila: ");
+            //string syöte = Console.Readline();
+            MessageBox.Show("Anna uusi lämpötila: ");
+            lämpötila = int.Parse(syöte);
+
+            //tiedostoon kirjoittaminen
+            string tiedosto = @"C:\Temp\Lämpötila.txt";
+            File.WriteAllText(tiedosto, lämpötila.ToString());
+            Console.WriteLine("Uusi lämpötila tallennettu.");
+
+            //odottaa enterin painamista
+            Console.ReadLine();
+        }
+    }
+}
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TiedonTallennusCA/TiedonTallennusCA/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiedonTallennusCA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lämpötila = 0;
+            //Console.Write("Anna uusi lämpötila: ");
+            //string syöte = Console.Readline();
+            lämpötila = int.Parse(syöte);
+
+            //tiedostoon kirjoittaminen
+            string tiedosto = @"C:\Temp\Lämpötila.txt";
+            File.WriteAllText(tiedosto, lämpötila.ToString());
+            Console.WriteLine("Uusi lämpötila tallennettu.");
+
+            //odottaa enterin painamista
+            Console.ReadLine();
+        }
+    }
+}
