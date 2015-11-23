1a5a5e96878bd4eacd402561a8844adf07fa4377
Johanna Merikanto <johanna.merikanto@stud.pointcollege.fi>
2015-11-23 15:12:20 +0200

    Toteutettu lämpötilan asettaminen ja tallennustiedoston.

--- TiedonTallennusCA/TiedonTallennusCA/Program.cs
+++ TiedonTallennusCA/TiedonTallennusCA/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace TiedonTallennusCA
 {
     class Program
     {
+        private static string syöte;
+
         static void Main(string[] args)
         {
             int lämpötila = 0;
